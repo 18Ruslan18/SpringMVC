4048b39ea4125f38b868da702ee5dc6594be0cd1
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-12 01:16:13 +0300

    startr on tomcat

--- src/main/java/controllers/UsersController.java
+++ src/main/java/controllers/UsersController.java
@@ -1,4 +1,41 @@
 package controllers;
 
-public class UsersController {
+import dao.UsersDao;
+import models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+public class UsersController implements Controller {
+
+    @Autowired
+    private UsersDao usersDao;
+
+
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        if (httpServletRequest.getMethod().equals("GET")) {
+            List<User> users = usersDao.findAll();
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("users");
+            modelAndView.addObject("usersFromServer", users);
+            return modelAndView;
+        }
+
+        return null;
+    }
+
+    @Override
+    public String value() {
+        return null;
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
+    }
 }
